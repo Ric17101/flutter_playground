03da2a036c2a07a5560b7f6ed38160a7b90bc7f7
Richard <ric17101raguine@gmail.com>
2024-12-11 17:56:32 +0800

    add docker widget sample and fix ios build

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_playground/others/dock_widget.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MainApp());
 }
 
@@ -9,10 +10,31 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Dock(
+            items: const [
+              Icons.person,
+              Icons.message,
+              Icons.call,
+              Icons.camera,
+              Icons.photo,
+            ],
+            builder: (e) {
+              return Container(
+                key: UniqueKey(),
+                height: 48,
+                constraints: const BoxConstraints(minWidth: 48),
+                margin: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.primaries[e.hashCode % Colors.primaries.length],
+                ),
+                child: Center(child: Icon(e, color: Colors.white)),
+              );
+            },
+          ),
         ),
       ),
     );
---
+++ lib/others/dock_widget.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+/// Dock of the reorderable [items].
+class Dock<T> extends StatefulWidget {
+  const Dock({
+    super.key,
+    this.items = const [],
+    required this.builder,
+  });
+
+  /// Initial [T] items to put in this [Dock].
+  final List<T> items;
+
+  /// Builder building the provided [T] item.
+  final Widget Function(T) builder;
+
+  @override
+  State<Dock<T>> createState() => _DockState<T>();
+}
+
+/// State of the [Dock] used to manipulate the [_items].
+class _DockState<T> extends State<Dock<T>> {
+  /// [T] items being manipulated.
+  late final List<T> items = widget.items.toList();
+
+  @override
+  void initState() {
+    items.sort((a, b) {
+      debugPrint('${a.toString()}: $a');
+      debugPrint('${b.toString()}: $b');
+
+      return (items.indexOf(a) > items.indexOf(b)) ? 1 : 0;
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Colors.black12,
+      ),
+      constraints: const BoxConstraints(minHeight: 48),
+      height: 70,
+      padding: const EdgeInsets.all(4),
+      child: ReorderableListView(
+        physics: const ClampingScrollPhysics(),
+        shrinkWrap: true,
+        buildDefaultDragHandles: false,
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        scrollDirection: Axis.horizontal,
+        proxyDecorator: (child, index, animation) {
+          return ScaleTransition(
+            scale: animation.drive(
+              Tween<double>(begin: 1, end: 1.4).chain(
+                CurveTween(curve: Curves.fastEaseInToSlowEaseOut),
+              ),
+            ),
+            child: child,
+          );
+        },
+        onReorder: (int oldIndex, int newIndex) {
+          setState(() {
+            debugPrint('newIndex: $newIndex');
+            debugPrint('oldIndex: $oldIndex');
+
+            if (newIndex > oldIndex) {
+              T item = items[oldIndex];
+              items.removeAt(oldIndex);
+              items.insert(newIndex - 1, item);
+            } else {
+              T item = items[oldIndex];
+              items.removeAt(oldIndex);
+              items.insert(newIndex, item);
+            }
+          });
+        },
+        children: items.map((e) {
+          return ReorderableDragStartListener(
+            key: UniqueKey(),
+            index: items.indexOf(e),
+            child: widget.builder(e),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
